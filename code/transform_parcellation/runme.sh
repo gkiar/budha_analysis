1db311d2dd1b7d5e35e6ba1343fe53df6483fc3c
Greg Kiar <gkiar07@gmail.com>
2019-05-11 08:18:15 -0400

    moved writedir to new location

--- code/transform_parcellation/runme.sh
+++ code/transform_parcellation/runme.sh
@@ -3,13 +3,17 @@
 # 1: participant label
 
 mountdir=~/ace_mount/ace_shared/bkhundrakpam/For_Greg/${1}/ALL_VOLS/
+outdir=~/ace_mount/ace_shared/gkiar/budha_analysis/${1}/
+
+mkdir -p ${outdir}
 
 docker run -ti \
            -v ${PWD}:${PWD} -w ${PWD} \
            -v ${mountdir}:/rundata/ \
+           -v ${outdir}:/outdata/ \
            --entrypoint ${PWD}/apply_xfm.sh \
            gkiar/dwipreproc_fsl-5.0.11_minified \
-           ./DK_res-2x2x2.nii.gz \
+           ${PWD}/DK_res-2x2x2.nii.gz \
            /rundata/MNI_2mm_to_DTI.mat \
-           /rundata/DK_in_DTI.nii.gz \
+           /outdata/DK_in_DTI.nii.gz \
            /rundata/dti_ec_brain.nii.gz
